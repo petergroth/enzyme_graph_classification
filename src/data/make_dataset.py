3bb9924ac829a73454aaf4ad55d233c29261da3e
jonasvj <jonas_vez@hotmail.com>
2021-06-10 15:10:08 +0200

    Add load_data func

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -16,16 +16,12 @@ def main(output_filepath):
         use_node_attr=True,
         use_edge_attr=True)
 
-    dataset = TUDataset(root=str(path)+'/data', name='ENZYMES', use_node_attr=True)
-
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
     data_dir = str(project_dir) + '/data/'
-    main(output_filepath=data_dir)
 
+    main(output_filepath=data_dir)
