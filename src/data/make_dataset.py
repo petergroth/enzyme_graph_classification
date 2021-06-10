8df8c1bcf82e749a53beeaf5c6dc99ea20519911
Peter Groth <petermoerchgroth@gmail.com>
2021-06-10 11:06:49 +0200

    added make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,17 +3,16 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+from torch_geometric.datasets import TUDataset
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(path):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    dataset = TUDataset(root=str(path)+'/data', name='ENZYMES', use_node_attr=True)
 
 
 if __name__ == '__main__':
@@ -27,4 +26,4 @@ if __name__ == '__main__':
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    main(project_dir)
