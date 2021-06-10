5f3aea167915c145848c78f127d4104eba9b2bbe
jonasvj <jonas_vez@hotmail.com>
2021-06-10 11:06:34 +0200

    Download TUD Enzymes data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from torch_geometric.datasets import TUDataset
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    
+    data = TUDataset(
+        root=output_filepath,
+        name='ENZYMES',
+        use_node_attr=True,
+        use_edge_attr=True)
 
 
 if __name__ == '__main__':
@@ -23,8 +24,5 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    data_dir = str(project_dir) + '/data/'
+    main(output_filepath=data_dir)
