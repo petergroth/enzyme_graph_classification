0204b88e6995e01e2296caac5a6bec70cbedf5b6
jonasvj <jonas_vez@hotmail.com>
2021-06-21 13:12:50 +0000

    Azure script for optimizing hparams

---
+++ azure_deployment/optimize_hparams.py
@@ -0,0 +1,42 @@
+# run-azure.py
+from azureml.core import Workspace
+from azureml.core import Experiment
+from azureml.core import Environment
+from azureml.core import ScriptRunConfig
+from azureml.core.conda_dependencies import CondaDependencies
+from azureml.core.run import Run
+from azureml.core import Model
+from wandb_api_key import WANDB_API_KEY, IMAGE
+import os
+
+COMPUTE_TARGET = 'MLOpsTest'
+
+if __name__ == "__main__":
+	# Setup experiment
+	ws = Workspace.from_config()
+	experiment = Experiment(workspace=ws, name='EGC_hyperparam_opt')
+
+	# Setup environment
+	env = Environment.from_docker_image(name="EGC_env", image=IMAGE)
+	env.environment_variables = {'WANDB_API_KEY': WANDB_API_KEY}
+
+	args = [
+		'--num_workers', 2,
+		'--n_startup_trials', 3,
+		'--n_warmup_steps', 50,
+		'--n_trials', 100,
+		'--timeout', 10000]
+	
+	# Define configuration file
+	config = ScriptRunConfig(source_directory='.',
+							 script='src/models/train_model_optuna.py',
+							 environment=env,
+							 arguments=args,
+							 compute_target=COMPUTE_TARGET)
+
+
+	# Submit experiment and wait for completion
+	run = experiment.submit(config)
+	run.wait_for_completion()
+
+	
